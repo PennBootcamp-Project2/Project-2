f719801e37f07f88f5e7c16fe4677656b329143d
Megan Pellegrino-Zubricky <pellegrino.megan@gmail.com>
2018-08-26 20:38:48 -0400

    with user updates

--- public/js/home.js
+++ public/js/home.js
@@ -20,3 +20,55 @@ loginButton.addEventListener('click', () => {
   userForms.classList.remove('bounceLeft')
   userForms.classList.add('bounceRight')
 }, false)
+
+
+
+
+$("#submit-signup").on("click", function(event) {
+  event.preventDefault();
+  var formValid = true;
+  $(".chosen-select").each(function() {
+    if ($(this).val() === "Select an Option") {
+      formValid = false;
+    }
+  });
+
+  if (formValid !== true) {
+    alert("You need to fill out all the fields before submitting!")
+    return;
+  }
+
+  var UserSignUp = {
+    email: $("#email").val(),
+    password: $("#password").val(),
+  };
+
+  $.post("/api/signup", UserSignUp, function(data) {
+    alert(data);
+  });
+});
+
+  $("#submit-login").on("click", function(event) {
+    event.preventDefault();
+    var formValid = true;
+    $(".chosen-select").each(function() {
+      if ($(this).val() === "Select an Option") {
+        formValid = false;
+      }
+    });
+
+    if (formValid !== true) {
+      alert("You need to fill out all the fields before submitting!")
+      return;
+    }
+
+    var UserSignUp = {
+      email: $("#email").val(),
+      password: $("#password").val(),
+    };
+
+    $.post("/api/login", UserSignUp, function(data) {
+      $("#email").text(data.name);
+      $("#password").text(data.name);  
+    });
+  });
