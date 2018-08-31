f6b405e0c3d5b81d347b27b34280baa4dd1d186a
Szsaunders <sadikisaunders@gmail.com>
2018-08-31 09:22:10 -0400

    Search results populated. Modal coming up with information. There's some stuff that I'll have to rewrite later once I figure out why then() isn't working right in these functions, but it should be functional enough.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -60,25 +60,29 @@ module.exports = function (app) {
     });
   });
 
-  app.post('/result', function (req, res) {
-      let input = req.body.items
-      // input.isbn = (req.body.items[0].volumeInfo.industryIdentifiers[0].identifier);
-      // input.title = (req.body.items[0].volumeInfo.title);
+  // app.post('http://localhost:3000/result', function (req, res) {
+  //     // let input = JSON.stringify(req.body.items)
+  //     // input.isbn = (req.body.items[0].volumeInfo.industryIdentifiers[0].identifier);
+  //     // input.title = (req.body.items[0].volumeInfo.title);
+  //   console.log(req.body)
 
-      console.log(input);
 
     // res.send(input)
-    res.render("result", input)
-  });
+    // res.render("result", input)
+  // });
 
-};
-=======
-  app.post('/result', function(req, res) {
-    var obj = {};
-    
-    console.log('body: ' + JSON.stringify(req.body));
-    res.send(req.body);
-  });
+  // app.post('/result', function(req, res) {
+  // //   var obj = {};
+  //   // var input = req.body.items;
+  //   var hbsObject = {
+  //     "input": req.body.items
+  //   };
+  //   console.log(hbsObject)
+  //   // console.log(input);
+  // //   // console.log('body: ' + JSON.stringify(req.body));
+  //   res.send("search");
+  // });
+
 
 };
 
