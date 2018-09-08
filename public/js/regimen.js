c4b36ced92d052f330577b6300d979ab534ad642
Szsaunders <sadikisaunders@gmail.com>
2018-09-08 10:35:12 -0400

    it's red now

--- public/js/regimen.js
+++ public/js/regimen.js
@@ -106,7 +106,7 @@ $( document ).ready(function() {
             var $button = $("<button>").attr("type","submit").text("Update").addClass("update-progress")
             
             var $progress = $("<div>")
-            .addClass("progress-bar-animated bg-info progress-bar progress-bar-striped")
+            .addClass("progress-bar-animated bg-danger progress-bar progress-bar-striped")
             .attr("aria-valuemin", 0)
             .attr("aria-valuemax", 100)
             .attr("id", "progressBar")
@@ -114,7 +114,7 @@ $( document ).ready(function() {
             .attr("aria-valuenow", pagePercentage)
             .text(bookTitle + ": " + pagePercentage + "%")
             .attr("role","progressbar");
-            var $div = $("<div>").text("Current progress:").addClass("progress").attr("style", "height: 40px").append($progress)
+            var $div = $("<div>").addClass("progress").attr("style", "height: 40px").append($progress)
             
             $("#regimenDisplay").append($h3).append($p).append($label).append($input).append($button).append($div)
         }
