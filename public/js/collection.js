2be5e89e1b8ca7846e98861800a281121472d345
pjmccall <pjmccall13@gmail.com>
2018-09-07 21:55:07 -0400

    delete working

--- public/js/collection.js
+++ public/js/collection.js
@@ -1,9 +1,9 @@
-$(document).ready(function () {
+$(document).ready(function() {
   var slideContainer = $("#slide-container");
   var slideList = $("#slide-list");
 
   $(document).on("click", "#button-delete", deleteBook);
-  // $(document).on("click", "#button-regimen", regimenBook);
+  $(document).on("click", "#button-regimen", regimenBook);
   var books = [];
 
   getBooks();
@@ -12,13 +12,13 @@ $(document).ready(function () {
     slideContainer.empty();
     var addCollection = [];
     for (var i = 0; i < books.length; i++) {
-      addCollection.push(addBook(books[i],i));
+      addCollection.push(addBook(books[i], i));
     }
     slideContainer.append(addCollection);
   }
 
   function getBooks() {
-    $.get("/api/books", function (data) {
+    $.get("/api/books", function(data) {
       console.log("Books", data);
       books = data;
       newListItems();
@@ -30,7 +30,7 @@ $(document).ready(function () {
     slideList.empty();
     var slideItem = [];
     for (var i = 0; i < books.length; i++) {
-      slideItem.push(addNewList(books[i],i));
+      slideItem.push(addNewList(books[i], i));
     }
     slideList.append(slideItem);
   }
@@ -41,14 +41,30 @@ $(document).ready(function () {
     $.ajax({
       method: "DELETE",
       url: "/api/books/" + id
-    }).then(books);
+    }).then(function() {
+      getBooks();
+    });
+  }
+
+  function regimenBook(book) {
+    $.ajax({
+      method: "POST",
+      url: "/api/books/current",
+      data: book
+    }).then(function() {
+      window.location.replace("/regimen");
+    });
   }
 
   function addBook(book, index) {
-    // Holds dynamic data 
-    var newList = $("<li data-target='#carousel-example' data-slide-to='0' class='active'>");
+    // Holds dynamic data
+    var newList = $(
+      "<li data-target='#carousel-example' data-slide-to='0' class='active'>"
+    );
 
-    var newSlide = $("<div class='carousel-item'><img class='d-block w-100' src='../img/Books03.jpg'><div class='mask rgba-black-light'>");
+    var newSlide = $(
+      "<div class='carousel-item'><img class='d-block w-100' src='../img/Books03.jpg'><div class='mask rgba-black-light'>"
+    );
     var activeSlide = "";
 
     var classSlide = index;
@@ -59,7 +75,7 @@ $(document).ready(function () {
     var newSlideCaption = $("<div class='carousel-caption'>");
     var newSlideView = $("<div class='text-center'>");
 
-    var newSlideImage = $("<img class='rounded' style='width: 300px; height: 350px'><br><br><br>");
+    var newSlideImage = $("<img class='rounded book-image' style='width: 300px; height: 350px'><br><br><br>");
     newSlideImage.attr("src", book.image_link);
     newSlideImage.attr("alt", book.title);
     var newSlideTitle = $("<h3 class='text-dark'>");
@@ -70,8 +86,12 @@ $(document).ready(function () {
     var newBuy = $("<a target='_blank'>");
     newBuy.text("Buy It Now");
     newBuy.attr("href", book.purchase_link);
-    var deleteButton = $("<br><br><button class='delete btn btn-dark btn-lg' id='button-delete'><span class='fa fa-eye'></span>Delete from Collection</button>&nbsp;&nbsp;");
-    var regimenButton = $("<button class='regimen btn btn-white btn-lg' id='button-regimen'><span class='fa fa-eye'></span>Move to Regimen</button>");
+    var deleteButton = $(
+      "<br><br><button class='delete btn btn-dark btn-lg' id='button-delete'><span class='fa fa-eye'></span>Delete from Collection</button>&nbsp;&nbsp;"
+    );
+    var regimenButton = $(
+      "<button class='regimen btn btn-white btn-lg' id='button-regimen'><span class='fa fa-eye'></span>Move to Regimen</button>"
+    );
     deleteButton.data("id", book.id);
     regimenButton.data("id", book.id);
 
@@ -86,13 +106,9 @@ $(document).ready(function () {
     newSlideCaption.append(deleteButton);
     newSlideCaption.append(regimenButton);
     return newSlide;
-
   }
 
-
-function addNewList(book, index) {
-
-
+  function addNewList(book, index) {
     var newList = $("<li data-target='#carousel-example'>");
     var dataClass = "";
 
@@ -110,5 +126,4 @@ function addNewList(book, index) {
     newList.addClass(dataClass);
     return newList;
   }
-
 });
